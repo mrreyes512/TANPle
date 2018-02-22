20631a67572cd6cb4be7202e9cc413c496d1e8a3
Mark Reyes <mark.reyes@charter.com>
2018-02-22 01:06:25 -0600

    testing

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
@@ -16,7 +17,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    # res = processRequest(req)
+    res = actionRoute(req)
     #
     # # res = json.dumps(res, indent=4)
     # # print(res)
@@ -26,6 +27,13 @@ def webhook():
     # r = make_response(json_convert)
     # r.headers['Content-Type'] = 'application/json'
     # return r
+
+
+def actionRoute(req):
+    if req.get("result").get("action") == "queryLine":
+        print("this is queryLine")
+        return
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
