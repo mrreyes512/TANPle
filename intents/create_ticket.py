58faa09d0812cd2fc064aa066bd977f302062bc6
Mark Reyes <mark.reyes@charter.com>
2018-03-02 19:04:31 -0600

    testing post_data print

--- intents/create_ticket.py
+++ intents/create_ticket.py
@@ -6,21 +6,28 @@ import app
 
 def post_data(req):
     # print(json_object)
-    contexts = req['result']['contexts']
-
-    # list compren
+    # list comprehension
     # [x * 2 for x in [1, 2, 3, 4]]  #=> [2, 4, 6, 8]
+
+    contexts = req['result']['contexts']
     name_list = [name['parameters'].get('given-name') for name in contexts
                  if name['parameters'].get('given-name') is not None]
 
     first_name = name_list[0]
+
+    issue_list = [name['parameters'].get('issue_sum') for name in contexts
+                 if name['parameters'].get('issue_sum') is not None]
+
+    issue_sum = issue_list[0]
 
 
     # issue_type = json_load['result']['contexts']['parameters']['given-name']
     # callback_method = json_load['result']['contexts']['parameters']['given-name']
     # callback_details = json_load['result']['contexts']['parameters']['given-name']
     # print("in post_data")
+    print(req)
     print("first_name:" + first_name)
+    print("issue_sum:" + issue_sum)
 
     # db_query = "SELECT ticket_id, first_name, issue_type FROM public.example_table"
     # result = app.db_connection(db_query)
